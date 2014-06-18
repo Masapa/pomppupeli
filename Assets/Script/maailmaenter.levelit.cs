5067de4466cf5f1cae43d8c249f410979b6b12b2
JK <joonas.kolppanen@luukku.com>
2014-06-18 14:39:43 +0300

    korjausta

--- Assets/Script/maailmaenter.levelit.cs
+++ Assets/Script/maailmaenter.levelit.cs
@@ -15,6 +15,7 @@ public partial class maailmaenter : MonoBehaviour {
 			levut.Add ("piikkipaikka1");
 			levut.Add ("alkupolku");
 			levut.Add ("Ilomaa_1");
+			levut.Add ("Ilomaa_2");
 			levut.Add ("level1");
 			levut.Add ("level2");
 			levut.Add ("level3");
